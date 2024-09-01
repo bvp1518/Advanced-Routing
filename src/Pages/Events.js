4e5f300bbfcce43d531adcab9d378e4cc1f3b62c
bvp1518 <bvp18061992@gmail.com>
2024-09-01 18:06:06 +0530

    till-361. Deferring Data Fetching with defer()

--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -29,27 +29,26 @@
 
 // export default EventsPage;
 
-import { useLoaderData,json } from 'react-router-dom';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-    const data = useLoaderData();
+    const { events } = useLoaderData();
 
-    // if(data.isError) {
-    //     return <p>{data.message}</p>
-    // }
-    const events = data.events;
-
-    // console.log("events",events);
-    
-
-    return <EventsList events={events} />
+    return (
+    <Suspense fallback={<p style={{textAlign:'center'}}>Loading...</p>}>
+     <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+    </Await> 
+    </Suspense>
+);
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents(){
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
@@ -62,6 +61,14 @@ export async function loader() {
     });
 
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.events;
     }
 }
+
+
+export function loader() {
+  return defer({
+      events: loadEvents()  
+    })
+   }
