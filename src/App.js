593ac388ed6963e71253b8c4f8ceaad862b34298
bvp1518 <bvp18061992@gmail.com>
2024-09-01 17:23:06 +0530

    till-360. Behind-the-Scenes Work with useFetcher()

--- src/App.js
+++ src/App.js
@@ -1,54 +1,18 @@
-// Challenge / Exercise
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// DONE
-
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// DONE
-
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// DONE
-
-// 4. Add properly working links to the MainNavigation.
-// DONE
-
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// DONE
-
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// DONE
-
-// 7. Output the ID of the selected event on the EventDetailPage
-// DONE
-
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-// DONE
-
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import HomePage from "./Pages/Home";
-import EventsPage, { loader as eventsLoader } from "./Pages/Events";
-import EventDetailPage, { 
+import EditEventPage from './Pages/EditEvent';
+import ErrorPage from './Pages/Error';
+import EventDetailPage, {
   loader as eventDetailLoader,
-  action as deleteEventAction
-} from "./Pages/EventDetail";
-import NewEventPage from "./Pages/NewEvent";
-import EditEventPage from "./Pages/EditEvent";
-import RootLayout from "./Pages/Root";
-import EventsRootLayout from "./Pages/EventsRoot";
-import ErrorPage from "./Pages/Error";
-import { action as manipulateEventAction } from'./components/EventForm';
+  action as deleteEventAction,
+} from './Pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './Pages/Events';
+import EventsRootLayout from './Pages/EventsRoot';
+import HomePage from './Pages/Home';
+import NewEventPage from './Pages/EventDetail';
+import RootLayout from './Pages/Root';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './Pages/Newsletter';
 
 const router = createBrowserRouter([
   {
@@ -64,31 +28,43 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: eventsLoader
+            loader: eventsLoader,
           },
           {
             path: ':eventId',
-            id:'event-detail',
+            id: 'event-detail',
             loader: eventDetailLoader,
             children: [
               {
                 index: true,
                 element: <EventDetailPage />,
-                action:deleteEventAction,
+                action: deleteEventAction,
               },
-              { path: 'edit', element: <EditEventPage />, action: manipulateEventAction }
-            ]
-          },        
-          { path: 'new', element: <NewEventPage />, action: manipulateEventAction },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
-])
-
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
